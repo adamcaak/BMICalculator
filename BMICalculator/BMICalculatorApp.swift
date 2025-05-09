b1030acea773b0907767a3484be7fd2dcd2c5399
adamcaak <ladamczak1997@gmail.com>
2025-05-09 22:40:21 +0200

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Łukasz Adamczak on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
