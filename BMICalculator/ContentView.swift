febe37b479549cc63e919d3b151d2deeb9118c62
adamcaak <ladamczak1997@gmail.com>
2025-05-09 23:02:41 +0200

    Create BMICalculator View
    It's simple view.

--- BMICalculator/ContentView.swift
+++ BMICalculator/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var weight: String = ""
+    @State private var height: String = ""
+    @State private var scoreBMI: Double = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("BMI Calculator")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                TextField("Waga", text: $weight)
+                    .padding()
+                    .keyboardType(.decimalPad)
+                    .background(Color(.gray).opacity(0.2))
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+                TextField("Wzrost", text: $height)
+                    .padding()
+                    .keyboardType(.decimalPad)
+                    .background(Color(.gray).opacity(0.2))
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+                Button(action: {
+                    //Logika
+                    print("BMI")
+                }) {
+                    Text("Oblicz BMI")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.orange)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
