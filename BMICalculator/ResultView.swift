21a9263d85cde957247abfc75878d80dbadb6211
adamcaak <ladamczak1997@gmail.com>
2025-05-13 23:27:38 +0200

    Create ResultView

---
+++ BMICalculator/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  BMICalculator
+//
+//  Created by Łukasz Adamczak on 13/05/2025.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
