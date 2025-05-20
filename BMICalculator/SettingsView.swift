62f9b2b240e32c3bc198956de5702a5e60c390ab
adamcaak <ladamczak1997@gmail.com>
2025-05-20 12:42:14 +0200

    Create SettingsView

---
+++ BMICalculator/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  BMICalculator
+//
+//  Created by Łukasz Adamczak on 20/05/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
