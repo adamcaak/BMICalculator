3fadf77be7ebe9179fd8919cce8eebe77a9ae0a8
adamcaak <ladamczak1997@gmail.com>
2025-05-20 12:41:44 +0200

    Add TabView

--- BMICalculator/MainTabView.swift
+++ BMICalculator/MainTabView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ResultView()
+                .tabItem {
+                    Label("Result", systemImage: "person.circle")
+                }
+            HistoryView() {
+                .tabItem {
+                    Label("History", systemImage: "clock.arrow.circlepath")
+                }
+            }
+            
+            SettingsView() {
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape")
+                }
+            }
+        }
     }
 }
 
