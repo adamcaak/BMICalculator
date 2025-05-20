05086068e9ea2dfcbb3c36deaf58828601fe65e0
adamcaak <ladamczak1997@gmail.com>
2025-05-20 12:29:02 +0200

    Create MainTabView

---
+++ BMICalculator/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  BMICalculator
+//
+//  Created by Łukasz Adamczak on 20/05/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
